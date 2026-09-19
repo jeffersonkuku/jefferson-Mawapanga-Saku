48f387fb8007542b1fb6fbae522783385a15991d
Jefferson Mawapanga Saku <jefferson.saku@gmail.com>
2026-09-19 10:40:15 +0200

    Refresh cache for iPhone audio start fix

--- roadwords-au/sw.js
+++ roadwords-au/sw.js
@@ -1,4 +1,4 @@
-const CACHE='roadwords-au-v3.0.0';
+const CACHE='roadwords-au-v4.0.0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install', event => {
