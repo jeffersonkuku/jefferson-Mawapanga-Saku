6fc9c65a1445fbf6b53b789de176ed954cfdd488
Jefferson Mawapanga Saku <jefferson.saku@gmail.com>
2026-09-20 02:19:46 +0200

    Reset RoadWords: remove roadwords-au/sw.js

--- roadwords-au/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install',event=>{self.skipWaiting();});
-self.addEventListener('activate',event=>{event.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)));}catch{}try{await self.registration.unregister();}catch{}try{await self.clients.claim();}catch{}})());});
-self.addEventListener('fetch',()=>{});
